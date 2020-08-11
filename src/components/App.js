1cccbd870cf21b2533b8ad525d9206beb7424689
anarubado <rubadoana@gmail.com>
2020-08-11 19:58:28 -0300

    uso de props en Box.js para inyectar contenido variable; habria que buscar una forma mas prolija de manejar los contenidos

--- src/components/App.js
+++ src/components/App.js
@@ -12,14 +12,34 @@ import Footer from './Footer';
 // Recordar reemplazar class por className
 
 function App() {
+  
+  var content =
+    <div>
+      <div className="text-center">
+        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" src="/images/product_dummy.svg" alt="dummy"/>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
+      <a target="_blank" rel="nofollow" href="/">View product detail</a>
+    </div>;
+
+  var content1 = 
+    <div className="row">                        
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+  </div>;
+
   return (
     <div id="wrapper">      
       <Sidebar />
       <div id="content-wrapper" className="d-flex flex-column">
         <div id="content">
           <Navbar />
-
           <div className="container-fluid">
+
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
               <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
             </div>
@@ -30,26 +50,9 @@ function App() {
               <Card />
             </div>
 
-
             <div className="row">
-              <Box />
-              <div className="col-lg-6 mb-4">
-                <div className="card shadow mb-4">
-                    <div className="card-header py-3">
-                        <h6 className="m-0 font-weight-bold text-primary">Last product in Data Dase</h6>
-                    </div>
-                    <div className="card-body">
-                      <div className="row">                        
-                        <Category />
-                        <Category />
-                        <Category />
-                        <Category />
-                        <Category />
-                        <Category />
-                      </div>
-                    </div>
-                </div>
-              </div>
+              <Box content={content}/>
+              <Box content={content1}/>
             </div>
 
           </div>
